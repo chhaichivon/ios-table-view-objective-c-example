904556df1e9f2127d4114dc36e232d23e96007b5
chhaichivon <chhaichivon1995@gmail.com>
2018-02-09 01:46:58 +0700

    Initial Commit

---
+++ TableViewDemoObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDemoObjectiveC
+//
+//  Created by Chhai Chivon on 2/9/18.
+//  Copyright © 2018 Chhai Chivon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewDemoObjectiveCTests/TableViewDemoObjectiveCTests.m
@@ -0,0 +1,39 @@
+//
+//  TableViewDemoObjectiveCTests.m
+//  TableViewDemoObjectiveCTests
+//
+//  Created by Chhai Chivon on 2/9/18.
+//  Copyright © 2018 Chhai Chivon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewDemoObjectiveCTests : XCTestCase
+
+@end
+
+@implementation TableViewDemoObjectiveCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
